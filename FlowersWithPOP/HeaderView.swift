cd293bfe70c1e44789453f9493bb7bca75b91c11
Mikael <miketekle14@gmail.com>
2016-09-26 15:25:50 -0700

    Created subclass for HeaderView and conformed to DrawShadow Protocol, connected to view in storyboard

---
+++ FlowersWithPOP/HeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderView.swift
+//  HeroesWithPOP
+//
+//  Created by Mikael Teklehaimanot on 9/26/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView, DrawShadow {
+
+}
