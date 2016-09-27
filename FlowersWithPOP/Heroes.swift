2a48fbce96f63890aa9f6353d94d7d295d84069b
Mikael <miketekle14@gmail.com>
2016-09-26 17:40:53 -0700

    created model file with struct for initialization

---
+++ FlowersWithPOP/Heroes.swift
@@ -0,0 +1,21 @@
+//
+//  Heroes.swift
+//  HeroesWithPOP
+//
+//  Created by Mikael Teklehaimanot on 9/26/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import Foundation
+
+
+struct Hero {
+    private var _id:Int!
+    private var _gender: String!
+    private var _age: Int!
+    private var _name: String!
+    private var _powerLevel: Int!
+    private var _specialAbility: String!
+    private var _affiliation: String!
+    
+}
