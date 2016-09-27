7298c5a713c3e1cc707786334705df44a7fb79b6
Mikael <miketekle14@gmail.com>
2016-09-26 21:03:21 -0700

    added more heroes for data service

--- FlowersWithPOP/DataService.swift
+++ FlowersWithPOP/DataService.swift
@@ -30,6 +30,14 @@ class DataService {
         heroesInSecretLair.append(Hero(id: 6, name: "Sora", gender: 2, age: 17, powerLevel: 5, specialAbility: "Keyblade Blast", _affiliation: "Solo"))
         heroesInSecretLair.append(Hero(id: 7, name: "Robin", gender: 2, age: 12, powerLevel: 5, specialAbility: "Acrobat Fury", _affiliation: "Flying Graysons"))
         heroesInSecretLair.append(Hero(id: 8, name: "Jean Grey/Phoenix", gender: 1, age: 21, powerLevel: 5, specialAbility: "Phoenix Force", _affiliation: "X-Men"))
+        heroesInSecretLair.append(Hero(id: 9, name: "Nightcrawler", gender: 2, age: 21, powerLevel: 4, specialAbility: "Teleportation Fury", _affiliation: "X-Men"))
+        heroesInSecretLair.append(Hero(id: 10, name: "Emma Frost", gender: 1, age: 28, powerLevel: 3, specialAbility: "Telepathic Knockout", _affiliation: "Hellfire Club"))
+        heroesInSecretLair.append(Hero(id: 11, name: "Rogue", gender: 1, age: 21, powerLevel: 4, specialAbility: "Power Drain", _affiliation: "X-Men"))
+        heroesInSecretLair.append(Hero(id: 12, name: "QuickSilver", gender: 2, age: 21, powerLevel: 4, specialAbility: "Speed Strike", _affiliation: "Brotherhood"))
+        heroesInSecretLair.append(Hero(id: 13, name: "Gambit", gender: 2, age: 25, powerLevel: 4, specialAbility: "Kinetic Cards", _affiliation: "X-Men"))
+        heroesInSecretLair.append(Hero(id: 14, name: "Batgirl", gender: 1, age: 19, powerLevel: 3, specialAbility: "Shadow Knockout", _affiliation: "Solo"))
+        heroesInSecretLair.append(Hero(id: 15, name: "Boy Chin Wonder", gender: 2, age: 8, powerLevel: 3, specialAbility: "Chin Attack", _affiliation: "Solo"))
+        heroesInSecretLair.append(Hero(id: 16, name: "Blue Ranger", gender: 2, age: 23, powerLevel: 3, specialAbility: "Spin Attack", _affiliation: "Power Rangers"))
         
         delegate?.addHeroToLair()
         
