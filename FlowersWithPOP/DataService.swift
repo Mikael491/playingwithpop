e6c29b839fa561320c464b463b309a6ed24c6f99
Mikael <miketekle14@gmail.com>
2016-09-26 20:02:25 -0700

    added file for data service class

---
+++ FlowersWithPOP/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  HeroesWithPOP
+//
+//  Created by Mikael Teklehaimanot on 9/26/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import Foundation
