92f2f955434093c1c86a590874a03bc13ab0542b
Mikael <miketekle14@gmail.com>
2016-09-27 13:01:10 -0700

    added file for custom cell class as well as nib for interface

---
+++ FlowersWithPOP/HeroCell.swift
@@ -0,0 +1,18 @@
+//
+//  HeroCell.swift
+//  HeroesWithPOP
+//
+//  Created by Mikael Teklehaimanot on 9/27/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+
+class HeroCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
