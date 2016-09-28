37d0cdf016d821962dc5319a15b4b09949d0a00a
Mikael <miketekle14@gmail.com>
2016-09-28 09:21:13 -0700

    added code to make UIView shake with animation

--- FlowersWithPOP/Shakeable.swift
+++ FlowersWithPOP/Shakeable.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol Shakeable {}
+extension Shakeable where Self: UIView {
+    
+    func shake() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 4.0, y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 4.0, y: self.center.y))
+        
+    }
+    
+}
