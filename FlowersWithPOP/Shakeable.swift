ba60f31441a1202b1c71a370e06c88483e2ccd43
Mikael <miketekle14@gmail.com>
2016-09-28 09:20:34 -0700

    added file for animation with protocol

---
+++ FlowersWithPOP/Shakeable.swift
@@ -0,0 +1,9 @@
+//
+//  Shakeable.swift
+//  HeroesWithPOP
+//
+//  Created by Mikael Teklehaimanot on 9/28/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import Foundation
