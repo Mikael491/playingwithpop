7bace0d68537da6544a0b1bf0838c377ff824479
Mikael <miketekle14@gmail.com>
2016-09-28 08:10:59 -0700

    added methods to collection view to remove need for strings as reuse identifiers & nib names

--- FlowersWithPOP/UICollectionView+Methods.swift
+++ FlowersWithPOP/UICollectionView+Methods.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+extension UICollectionView {
+    
+    func register<T: UICollectionViewCell>(_: T.Type) where T: ReuseableCell {
+        
+        let nib = UINib(nibName: T.nibName, bundle: nil)
+        register(nib, forCellWithReuseIdentifier: T.nibName)
+    }
+    
+    func dequeReuseableCell<T: UICollectionViewCell where T: ReuseableCell>(forIndexPath indexPath: NSIndexPath) -> T {
+    
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.nibName, for: indexPath as IndexPath) as? HeroCell else {
+            print("=======================================================")
+            print("=======================================================")
+            fatalError("There was an error at guard statement in UICollectionView Extension file")
+        }
+        
+        return cell as! T
+    }
+}
